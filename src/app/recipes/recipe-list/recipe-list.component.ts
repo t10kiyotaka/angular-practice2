8a53f5905f4194bcceb3feb3aee5d3dab0a2a3a9
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-17 00:01:28 +0900

    Make recipe-list VIEW

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test recipe.',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
