88eb03b3c9e03096b1478cb06c90ed72a3d11613
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-18 23:54:34 +0900

    Add Ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a test recipe.',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test recipe', 'This is a test recipe.',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
