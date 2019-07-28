5dcf36cb027e24b71c6ccfd6430a7d9baab56efb
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-28 15:04:05 +0900

    Modify recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a test recipe.',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test recipe', 'This is a test recipe.',
+    new Recipe('Another test recipe', 'This is a test recipe.',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
