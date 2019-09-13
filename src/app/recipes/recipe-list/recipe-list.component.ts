9a54106db6b4d56310ad9569514626a1c9c1ac45
Kiyotaka <t10kiyotaka@gmail.com>
2019-09-13 23:49:46 +0900

    Add RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,12 +9,7 @@ import { Recipe } from "../recipe.model";
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test recipe.',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another test recipe', 'This is a test recipe.',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  ];
+
 
   constructor() {
 
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import {Recipe} from "./recipe.model";
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test recipe.',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Another test recipe', 'This is a test recipe.',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
