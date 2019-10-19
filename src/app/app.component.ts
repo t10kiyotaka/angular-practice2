4fe74db198d5a53f0733deff84636fb2eb366d5a
Kiyotaka <t10kiyotaka@gmail.com>
2019-10-19 10:59:40 +0900

    Remove unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-practice2';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-
 }
