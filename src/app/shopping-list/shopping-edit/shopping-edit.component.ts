1965e35a981ff32ddf97f704676d8f5a52ae2845
Kiyotaka <t10kiyotaka@gmail.com>
2019-11-11 22:26:55 +0900

    Impl Clear button

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -37,7 +37,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  addIngredient(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.isEditMode) {
@@ -45,6 +45,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
+    this.isEditMode = false;
+    this.form.reset();
+  }
+
+  onClear() {
+    this.form.reset();
+    this.isEditMode = false;
   }
 
 }
